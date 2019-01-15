6d03b5e1276bde0949a69080308efaa06cdb370b
杨绪 <yangxuilyx@hotmail.com>
2019-01-15 18:46:38 +0800

    1.添加consul

--- User.Identity/Services/UserService.cs
+++ User.Identity/Services/UserService.cs
@@ -1,37 +1,50 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using DnsClient;
+using Microsoft.Extensions.Options;
+using User.Identity.Dtos;
 
 namespace User.Identity.Services
 {
     public class UserService : IUserService
     {
-        private readonly string _userServiceUrl = "http://localhost:5001/";
+        private readonly string _userServiceUrl;
         private HttpClient _httpClient;
 
-        public UserService(HttpClient httpClient)
+        public UserService(HttpClient httpClient, IDnsQuery dnsQuery, IOptions<ServiceDiscoveryOptions> serviceOptions)
         {
-            _httpClient = httpClient;
+             _httpClient = httpClient;
+
+            var address = dnsQuery.ResolveService("service.consul", serviceOptions.Value.UserServiceName);
+            var addressList = address.First().AddressList;
+
+            var host = addressList.Any() ? addressList.First().ToString() : address.First().HostName;
+            var port = address.First().Port;
+
+            _userServiceUrl = $"http://{host}:{port}/";
+
         }
 
-        public async Task<int> CheckOrCreate(string phone)
-        {
-            var formUrlEncodedContent = new FormUrlEncodedContent(new Dictionary<string, string>()
+    public async Task<int> CheckOrCreate(string phone)
+    {
+        var formUrlEncodedContent = new FormUrlEncodedContent(new Dictionary<string, string>()
             {
                 {"phone", phone}
             });
-            var response = await _httpClient.PostAsync(_userServiceUrl + "api/user/check-or-create", formUrlEncodedContent);
+        var response = await _httpClient.PostAsync(_userServiceUrl + "api/user/check-or-create", formUrlEncodedContent);
 
-            if (response.IsSuccessStatusCode)
-            {
-                var userId = await response.Content.ReadAsStringAsync();
+        if (response.IsSuccessStatusCode)
+        {
+            var userId = await response.Content.ReadAsStringAsync();
 
-                int.TryParse(userId, out int intUserId);
+            int.TryParse(userId, out int intUserId);
 
-                return intUserId;
-            }
-
-            return 0;
+            return intUserId;
         }
+
+        return 0;
     }
+}
 }
--- User.Identity/Startup.cs
+++ User.Identity/Startup.cs
@@ -1,15 +1,25 @@
 ﻿using System.Net.Http;
+using DnsClient;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using User.Identity.Authentication;
+using User.Identity.Dtos;
 using User.Identity.Services;
 
 namespace User.Identity
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -20,6 +30,16 @@ namespace User.Identity
                 .AddInMemoryClients(Config.GetClients());
 
             services.AddSingleton<HttpClient>(new HttpClient());
+
+            services.Configure<ServiceDiscoveryOptions>(Configuration.GetSection("ServiceDiscovery"));
+
+            services.AddSingleton<IDnsQuery>(p =>
+            {
+                var serviceOptions = p.GetService<IOptions<ServiceDiscoveryOptions>>();
+
+                return new LookupClient(serviceOptions.Value.Consul.DnsEndpoint.ToIPEndPoint());
+            });
+
             services.AddScoped<IAuthService, TestAuthService>();
             services.AddScoped<IUserService, UserService>();
         }
